bba28f5190e34f5474d3f53b7907f62579f8d3be
Wendel Santos <wendel.santos@consultores.rnp.br>
2024-05-21 15:08:19 -0300

    crud save

--- src/main/java/br/com/todo/manager/modules/user/service/UserService.java
+++ src/main/java/br/com/todo/manager/modules/user/service/UserService.java
@@ -1,7 +1,6 @@
 package br.com.todo.manager.modules.user.service;
 
 import br.com.todo.manager.database.entities.User;
-import br.com.todo.manager.database.repositories.RoleRepository;
 import br.com.todo.manager.model.dtos.UserDto;
 import br.com.todo.manager.model.dtos.UserResponseDto;
 import br.com.todo.manager.modules.user.provider.UserProvider;
@@ -13,8 +12,7 @@ public class UserService {
 
     @Autowired
     private UserProvider userProvider;
-    @Autowired
-    private RoleRepository roleRepository;
+
 
     public UserResponseDto createUser(UserDto userDto) {
         User user = new User(userDto);
